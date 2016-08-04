ad245dbb3fe6aaae9af8c7143928a8062d0119ea
Peter Jang <peter.x.jang@gmail.com>
2016-08-04 08:07:25 -0500

    Fix seed file emails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 names = ['Test', 'Bob', 'Jane', 'Alice']
 names.each do |name|
-  user = User.create(name: name, email: "#{name}@email.com", password: "password")
+  user = User.create(name: name, email: "#{name}@email.com".downcase, password: "password")
   3.times do
     Message.create(body: Faker::Lorem.paragraph, user_id: user.id)
   end
