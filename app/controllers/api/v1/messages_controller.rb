d2e7cd19573e83cba626bff95ba62ac4ec9e3b68
Peter Jang <peter.x.jang@gmail.com>
2016-08-04 08:39:21 -0500

    Broadcast message info on message create

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -9,6 +9,12 @@ class Api::V1::MessagesController < ApplicationController
       user_id: current_user.id,
       body: params[:body]
     )
+    ActionCable.server.broadcast 'activity_channel', {
+      id: @message.id,
+      name: @message.user.name,
+      body: @message.body,
+      created_at: @message.created_at
+    }
     render 'show.json.jbuilder'
   end
 end
