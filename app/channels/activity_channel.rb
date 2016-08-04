1f8fe18b7ff8ac2bcf0cc600b1bb36f8ed85e8df
Peter Jang <peter.x.jang@gmail.com>
2016-08-04 08:29:58 -0500

    Subscribe to activity channel on server side

--- app/channels/activity_channel.rb
+++ app/channels/activity_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ActivityChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "activity_channel"
   end
 
   def unsubscribed
